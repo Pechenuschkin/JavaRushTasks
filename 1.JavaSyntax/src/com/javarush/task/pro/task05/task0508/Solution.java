ef9615a7b702f826310dd208c6b856c8ebed1399
pechenuschkin <https://github.com/Pechenuschkin357@ya.ru/helloworld.git>
2026-04-15 18:47:35 +0300

    Add task0504-0515

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.pro.task05.task0508;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+/* 
+Удаляем одинаковые строки
+*/
+
+public class Solution {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        strings = new String[6];
+
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = scanner.nextLine();
+        }
+        for (int i = 0; i < strings.length; i++) {
+            int number = 0;
+            for (int j = 0; j < strings.length; j++) {
+
+                if (strings[i] != null && strings[j] != null) {
+                    if (strings[i].equals(strings[j]) && i != j) {
+                        strings[j] = null;
+                        ++number;
+                    }
+
+                }
+
+                }
+            if (number > 0){
+                strings[i] = null;
+            }
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
+    }
+}
