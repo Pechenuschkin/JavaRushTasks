9cfe35908144a714140833e5273d6cd545785d94
pechenuschkin <https://github.com/Pechenuschkin357@ya.ru/helloworld.git>
2026-05-18 21:33:32 +0300

    Add task1101-1128

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1122/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task11.task1122;
+
+/* 
+Затенение поля класса
+*/
+
+public class Solution {
+    public static int salary;
+
+    public static void add(int increase) {
+        int salary = Solution.salary;
+        salary += increase;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        add(100500);
+
+        System.out.println(salary);
+
+    }
+}
